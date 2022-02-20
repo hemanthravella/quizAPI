f7bc5455aec5ab9ca75f6a096bdc9b621f42e521
Hemanth Ravella <hemanthravella1729@gmail.com>
2022-02-20 14:16:09 -0500

    Halfway models

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Quizzes(models.Model):
+
+    class Meta:
+        verbose_name = _("Quiz")
+
+
+    category = models.ForeignKey(Category, default=1, on_delete=models.DO_NOTHING)
+
+
+class Question(UpdatedQuestion):
+    quiz = models.ForeignKey(Quizzes, related_name='question',on_delete=models.DO_NOTHING)
+
+
+class Answer(UpdatedQuestion):
+    question = models.ForeignKey(Question, related_name='answer', on_delete=models.DO_NOTHING)
